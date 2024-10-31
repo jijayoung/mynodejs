cb23ed7893e49fa608fe575a5835a7be8b270ef7
jjyc <jjyc.ui@gmail.com>
2024-10-31 10:23:25 +0900

    241030_updated

---
+++ 08/ch08_02/models/comment.js
@@ -0,0 +1,24 @@
+module.exports = ( sequelize, DataTypes ) => {
+
+    // create table Comments {
+    //     id integer primary key increment,
+    //     content text,
+    //     postId integer
+    //     foreign key (postId) references Posts(id)
+    // }
+
+    const Comment = sequelize.define("Comment",{
+        id : {
+            type: DataTypes.INTEGER,
+            allowNull :false,
+            primaryKey: true,
+            autoIncrement : true,
+        },
+        content : DataTypes.STRING
+    });
+    
+    Comment.associate = function(models){
+        models.Comment.belongsTo(models.Post)
+    }
+    return Comment;
+}
---
+++ 08/ch08_02/models/post.js
@@ -0,0 +1,24 @@
+module.exports = ( sequelize, DataTypes ) => {
+
+    // create table Posts {
+    //     id integer primary key increment,
+    //     title TEXT not null,
+    //     content text,
+    //     author text
+    // }
+
+    const Post = sequelize.define("Post",{
+        title : {
+            type: DataTypes.STRING,
+            allowNull :false
+        },
+        content : DataTypes.STRING,
+        author: DataTypes.STRING
+    });
+    
+    Post.associate = function(models){
+        Post.hasMany(models.Comment)
+    }
+
+    return Post;
+}
